33dca857c7a74240e2bf7999c14d97a8707d04cf
Artem <korobeninartem86@gmail.com>
2026-04-30 08:26:28 +0300

    FIX Exception: Add UnauthorizedException and fix handleException

--- src/main/java/com/weather/weatherviewer/exception/GlobalExceptionHandler.java
+++ src/main/java/com/weather/weatherviewer/exception/GlobalExceptionHandler.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public String handleException(Exception e, Model model) {
-        model.addAttribute("errorMessage", e.getMessage());
+        e.printStackTrace();
+        model.addAttribute("errorMessage","Something went wrong. Please try again later.");
         return "error";
 
+    }
+    @ExceptionHandler(UnauthorizedException.class)
+    public String handleUnauthorized(UnauthorizedException ex){
+        return "redirect:/login";
     }
 }
